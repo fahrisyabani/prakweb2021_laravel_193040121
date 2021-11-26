64aea673a4b2338e22d5ab1ab8ef1f85bb60da4b
fahrisyabani <fahrisyabanipd@gmail.com>
2021-11-26 17:43:07 +0700

    Menyelesaikan Video 4 (Blade Templating Engine)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Foreach_;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +15,73 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Fahri Syabani",
         "email" => "fahrisyabanipd@gmail.com",
         "image" => "Fahri Syabani.jpeg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fahri Syabani",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia. Doloremque delectus repellendus pariatur autem necessitatibus molestias? Ex provident ipsam mollitia aperiam autem excepturi quo quaerat dolorum beatae quibusdam? Maiores ab facilis reprehenderit neque molestias magnam cum incidunt autem nihil qui atque ipsa nam quam impedit quo ullam ex in doloremque, aperiam magni aspernatur excepturi enim laborum? Maiores dignissimos similique tempora, minus quasi nam in nostrum error voluptates dolores explicabo ullam. Totam itaque explicabo similique maxime delectus fugiat nam."
+        ],
+
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Warman",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia. Doloremque delectus repellendus pariatur autem necessitatibus molestias? Ex provident ipsam mollitia aperiam autem excepturi quo quaerat dolorum beatae quibusdam? Maiores ab facilis reprehenderit neque molestias magnam cum incidunt autem nihil qui atque ipsa nam quam impedit quo ullam ex in doloremque, aperiam magni aspernatur excepturi enim laborum? Maiores dignissimos similique tempora, minus quasi nam in nostrum error voluptates dolores explicabo ullam. Totam itaque explicabo similique maxime delectus fugiat nam, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia."
+        ],
+
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fahri Syabani",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia. Doloremque delectus repellendus pariatur autem necessitatibus molestias? Ex provident ipsam mollitia aperiam autem excepturi quo quaerat dolorum beatae quibusdam? Maiores ab facilis reprehenderit neque molestias magnam cum incidunt autem nihil qui atque ipsa nam quam impedit quo ullam ex in doloremque, aperiam magni aspernatur excepturi enim laborum? Maiores dignissimos similique tempora, minus quasi nam in nostrum error voluptates dolores explicabo ullam. Totam itaque explicabo similique maxime delectus fugiat nam."
+        ],
+
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Warman",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia. Doloremque delectus repellendus pariatur autem necessitatibus molestias? Ex provident ipsam mollitia aperiam autem excepturi quo quaerat dolorum beatae quibusdam? Maiores ab facilis reprehenderit neque molestias magnam cum incidunt autem nihil qui atque ipsa nam quam impedit quo ullam ex in doloremque, aperiam magni aspernatur excepturi enim laborum? Maiores dignissimos similique tempora, minus quasi nam in nostrum error voluptates dolores explicabo ullam. Totam itaque explicabo similique maxime delectus fugiat nam, dolor sit amet consectetur adipisicing elit. Deserunt, omnis officia."
+        ],
+
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
