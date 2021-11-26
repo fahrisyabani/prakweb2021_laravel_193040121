0c608a621118518498b6a1d7435b4e99977b99e1
fahrisyabani <fahrisyabanipd@gmail.com>
2021-11-26 23:38:16 +0700

    Menyelesaikan Video 9 (Database Seeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Fahri Syabani',
+            'email' => 'fahrisyabanipd@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Warman',
+            'email' => 'warman@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro, exercitationem odio quae expedita ipsa, est atque impedit optio? Voluptatum enim minima itaque cupiditate omnis magnam. Vitae, perferendis debitis tempora facilis animi aspernatur. Hic mollitia, cupiditate illo quisquam necessitatibus enim neque labore ducimus molestias consequuntur accusantium vel facere laborum explicabo quidem similique aspernatur blanditiis sunt tempore exercitationem. Autem voluptate excepturi dolorum quasi tempore maxime praesentium neque placeat exercitationem fugit architecto, quia magni aliquid. Nobis corporis maiores architecto tempore quibusdam placeat',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro, exercitationem odio quae expedita ipsa, est atque impedit optio? Voluptatum enim minima itaque cupiditate omnis magnam. Vitae, perferendis debitis tempora facilis animi aspernatur. Hic mollitia, cupiditate illo quisquam necessitatibus enim neque labore ducimus molestias consequuntur accusantium vel facere laborum explicabo quidem similique aspernatur blanditiis sunt tempore exercitationem. Autem voluptate excepturi dolorum quasi tempore maxime praesentium neque placeat exercitationem fugit architecto, quia magni aliquid. Nobis corporis maiores architecto tempore quibusdam placeat',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro, exercitationem odio quae expedita ipsa, est atque impedit optio? Voluptatum enim minima itaque cupiditate omnis magnam. Vitae, perferendis debitis tempora facilis animi aspernatur. Hic mollitia, cupiditate illo quisquam necessitatibus enim neque labore ducimus molestias consequuntur accusantium vel facere laborum explicabo quidem similique aspernatur blanditiis sunt tempore exercitationem. Autem voluptate excepturi dolorum quasi tempore maxime praesentium neque placeat exercitationem fugit architecto, quia magni aliquid. Nobis corporis maiores architecto tempore quibusdam placeat',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus perspiciatis sint sequi? Cum, blanditiis numquam iusto porro, exercitationem odio quae expedita ipsa, est atque impedit optio? Voluptatum enim minima itaque cupiditate omnis magnam. Vitae, perferendis debitis tempora facilis animi aspernatur. Hic mollitia, cupiditate illo quisquam necessitatibus enim neque labore ducimus molestias consequuntur accusantium vel facere laborum explicabo quidem similique aspernatur blanditiis sunt tempore exercitationem. Autem voluptate excepturi dolorum quasi tempore maxime praesentium neque placeat exercitationem fugit architecto, quia magni aliquid. Nobis corporis maiores architecto tempore quibusdam placeat',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
+    
         <h1 class="mb-5">{{ $post->title }}</h1>
 
-        <p>By. Fahri Syabani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> 
+            in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+                {{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     
-    </article>
+   
 
-    <a href="/posts">Back To Posts</a>
+    <a href="/posts" class="d-block mt-3">Back To Posts</a>
     
 @endsection
