56129bd08b63de2fe18b15f0791814fa5d1fb887
fahrisyabani <fahrisyabanipd@gmail.com>
2021-11-26 22:50:21 +0700

    Menyelesaikan Video 8 (Post Category & Eloquent Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use PhpParser\Node\Stmt\Foreach_;
@@ -38,3 +39,20 @@ Route::get('/posts', [PostController::class, 'index']);
 // Terhubung ke halaman PostController
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+// Route untuk categories
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+// 
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
