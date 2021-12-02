6610f03e90e16c0ebc19a757d55c7be74c7afffe
fahrisyabani <fahrisyabanipd@gmail.com>
2021-12-02 07:38:25 +0700

    Menyelesaikan Video 14 (View Login & Registration)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\User;
-use Illuminate\Support\Facades\Route;
-use App\Models\Post;
 use PhpParser\Node\Stmt\Foreach_;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +51,7 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
